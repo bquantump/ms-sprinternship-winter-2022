161fb65d24a655cb00b82f334e6572e9f1ea03a7
Johanna Rivera <johrivera@microsoft.com>
2022-01-13 00:15:53 +0000

    run pip install

--- eng/scripts/update_oot_modules.sh
+++ eng/scripts/update_oot_modules.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/microsoft/azure-software-radio.git
 cd azure-software-radio/gr-azure-software-radio
 git checkout d3c9ea2
+pip3 install -r python/requirements.txt
 mkdir build; cd build; cmake ..
 sudo make install -j
 
