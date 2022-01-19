a9355f58f6d5d295bef7fa1d7ea9d280541538ad
jo-rivera <johanna.rivera2@outlook.com>
2022-01-18 22:44:16 -0800

    update install scripts

--- eng/scripts/install_main.py
+++ eng/scripts/install_main.py
@@ -1,7 +1,10 @@
 import subprocess
+import sys
+
+branch = sys.argv[1] if len(sys.argv) > 1 else 'dev'
 
 clone = 'git clone https://github.com/microsoft/azure-software-radio.git'
-cd = 'cd azure-software-radio/gr-azure-software-radio; git checkout dev;' \
+cd = f'cd azure-software-radio/gr-azure-software-radio; git checkout {branch};' \
      'pip3 install -r python/requirements.txt; mkdir build; cd build; cmake ..; sudo make install -j'
 
 subprocess.check_call(clone, shell=True)
